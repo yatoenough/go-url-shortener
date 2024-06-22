cdc40dcb5f1e54f11b02783f547a56476a4a29db
yatoenough <yatoenough@gmail.com>
2024-06-22 21:28:36 +0200

    implemented redirecting by alias

--- internal/server/handlers/url/url.go
+++ internal/server/handlers/url/url.go
@@ -1,11 +1,14 @@
 package url
 
 import (
+	"errors"
 	"log/slog"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"github.com/go-playground/validator/v10"
+	"github.com/yatoenough/go-url-shortener/internal/db"
 	"github.com/yatoenough/go-url-shortener/internal/db/postgres"
 	"github.com/yatoenough/go-url-shortener/internal/lib/api/response"
 	"github.com/yatoenough/go-url-shortener/internal/lib/logger/attrs"
@@ -30,14 +33,14 @@ func (uh *UrlsHandler) Shorten(w http.ResponseWriter, r *http.Request) {
 
 	err := render.DecodeJSON(r.Body, &body)
 	if err != nil {
-		uh.logger.Error("failed to decode request", attrs.ErrAttr(err))
-		render.JSON(w, r, response.Error(http.StatusBadRequest, "failed to decode request"))
+		uh.logger.Info("failed to decode request", attrs.ErrAttr(err))
+		render.JSON(w, r, response.Error(http.StatusBadRequest, "Failed to decode request."))
 		return
 	}
 
 	if err := validator.New().Struct(body); err != nil {
-		uh.logger.Error("invalid url", attrs.ErrAttr(err))
-		render.JSON(w, r, response.Error(http.StatusBadRequest, "invalid url"))
+		uh.logger.Info("invalid url", attrs.ErrAttr(err))
+		render.JSON(w, r, response.Error(http.StatusBadRequest, "Invalid URL."))
 		return
 	}
 
@@ -45,13 +48,28 @@ func (uh *UrlsHandler) Shorten(w http.ResponseWriter, r *http.Request) {
 	_, err = uh.storage.SaveURL(body.URL, alias)
 	if err != nil {
 		uh.logger.Error("error while saving to DB", attrs.ErrAttr(err))
-		render.JSON(w, r, response.Error(http.StatusInternalServerError, "error while saving to DB"))
+		render.JSON(w, r, response.Error(http.StatusInternalServerError, "Internal server error."))
 		return
 	}
 
 	render.JSON(w, r, dto.URLResponse{Response: response.OK(), Alias: alias})
 }
 
-func (uh *UrlsHandler) Redirect() {
+func (uh *UrlsHandler) Redirect(w http.ResponseWriter, r *http.Request) {
+	alias := chi.URLParam(r, "alias")
 
+	resURL, err := uh.storage.GetURL(alias)
+	if err != nil {
+		if errors.Is(err, db.ErrURLNotFound) {
+			uh.logger.Info("url not found")
+			render.JSON(w, r, response.Error(http.StatusBadRequest, "URL not found."))
+			return
+		}
+
+		uh.logger.Error("failed to get url")
+		render.JSON(w, r, response.Error(http.StatusInternalServerError, "Internal server error."))
+		return
+	}
+
+	http.Redirect(w, r, resURL, http.StatusPermanentRedirect)
 }
--- internal/server/routes/routes.go
+++ internal/server/routes/routes.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 	"github.com/yatoenough/go-url-shortener/internal/db/postgres"
+	"github.com/yatoenough/go-url-shortener/internal/lib/api/response"
 	"github.com/yatoenough/go-url-shortener/internal/server/handlers/url"
 )
 
@@ -20,6 +22,10 @@ func RegisterRoutes(storage *postgres.Storage, logger *slog.Logger) http.Handler
 
 	urlHandler := url.New(storage, logger)
 
-	r.Post("/api/shorten", urlHandler.Shorten)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		render.JSON(w, r, response.Error(http.StatusNotFound, "Route not found."))
+	})
+	r.Post("/shorten", urlHandler.Shorten)
+	r.Get("/{alias}", urlHandler.Redirect)
 	return r
 }
