dd6e227e8d78e3fc5e1ef24ef706be8994a6a113
yatoenough <yatoenough@gmail.com>
2024-06-22 17:10:38 +0200

    setuped logger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/yatoenough/go-url-shortener/internal/config"
 )
 
+const (
+	envLocal = "local"
+	envDev   = "development"
+	envProd  = "production"
+)
+
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+
+	log := initLogger(cfg.Env)
+	log.Info("Starting application...")
+}
+
+func initLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
 }
